f2631e23dbfb98ed613d93b9f726592ece3e195c
DumbDane <lrpe@itu.dk>
2025-01-13 18:43:41 +0100

    Add configuration for zsh

--- hosts/ninox/configuration.nix
+++ hosts/ninox/configuration.nix
@@ -95,6 +95,9 @@
 
   # Install firefox.
   programs.firefox.enable = true;
+  environment.shells = with pkgs; [ zsh ];
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -107,6 +110,10 @@
     discord
     # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+  ];
+
+  fonts.packages = [
+    pkgs.nerd-fonts.jetbrains-mono
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
