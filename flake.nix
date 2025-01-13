38bba56bad71734336b692d4f337fbf13c80be91
DumbDane <lrpe@itu.dk>
2025-01-13 17:12:09 +0100

    inital commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+
+  description = "My Flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable"; # github:NixOS/nixpkgs/nixos-unstable
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {self, nixpkgs, home-manager, ...}:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+
+    in
+    {
+    nixosConfigurations = 
+      {
+      Ninox = lib.nixosSystem {
+        inherit system;
+        # specialArgs = { inherit system; };
+        modules = [ ./hosts/ninox/configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+	robert = home-manager.lib.homeManagerConfiguration {
+	  inherit pkgs;
+	  modules = [ ./home.nix ];
+	};
+
+    };
+
+  };
+
+}
