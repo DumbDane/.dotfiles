debda8c02f6e55cd9735393d7c7dddb01b4d3fc0
DumbDane <lrpe@itu.dk>
2025-01-22 10:31:56 +0100

    Update .zshrc to detect when mac, update darwin to use homebrew and mac-app-util with automatic adding apps to finder

--- .zshrc
+++ .zshrc
@@ -7,9 +7,10 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-
 # For mac
-# eval "$(/opt/homebrew/bin/brew shellenv)"
+if [ "$OSTYPE" = "darwin*" ]; then 
+	eval "$(/opt/homebrew/bin/brew shellenv)"; 
+fi
 
 # Set the directory we want to store zinit and plugings
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
--- charles/flake.nix
+++ charles/flake.nix
@@ -5,49 +5,51 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    mac-app-util.url = "github:hraban/mac-app-util";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, mac-app-util }:
   let
     configuration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
+      nixpkgs.config.allowUnfree = true;
       environment.systemPackages = with pkgs;
         [ 
+	  zsh
 	  neovim
 	  tmux
 	  zoxide
 	  fzf
 	  stow
-	 # ghostty # Broken rn, installed with homebrew instead
-	 # alacritty
-	 mkalias
-
-
+	  obsidian
+	  python3
+	  spotify
+	  discord
         ];
 
-      fonts.packages = [ pkgs.nerd-fonts.jetbrains-mono ];
 
-      system.activationScripts.applications.text = let
-      env = pkgs.buildEnv {
-      name = "system-applications";
-      paths = config.environment.systemPackages;
-      pathsToLink = "/Applications";
-      
+      environment.shellAliases = { python = "python3"; };
+
+      homebrew = {
+        enable = true;
+	onActivation = {
+          autoUpdate = true;
+	  upgrade = true;
+	  cleanup = "zap";
+	};
+	brews = [ "mas" ];
+	casks = [
+	  "hammerspoon"
+	  "ghostty"
+	];
+	masApps = { };
+
       };
-      in
-      pkgs.lib.mkForce ''
-	# Set up applications.
-	echo "setting up /Applications..." >&2
-	rm -rf /Applications/Nix\ Apps
-	mkdir -p /Applications/Nix\ Apps
-	find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-	while read -r src; do
-	app_name=$(basename "$src")
-	echo "copying $src" >&2
-	${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-	done
-      '';
+
+      fonts.packages = [ pkgs.nerd-fonts.jetbrains-mono ];
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
@@ -70,7 +72,19 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Macaw
     darwinConfigurations."Macaw" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration 
+	mac-app-util.darwinModules.default
+	nix-homebrew.darwinModules.nix-homebrew
+	{
+	  nix-homebrew = { 
+	    enable = true;
+	    enableRosetta = true;
+	    user = "lauridspedersen";
+	    autoMigrate = true;
+	  };
+	}
+      ];
     };
   };
 }
