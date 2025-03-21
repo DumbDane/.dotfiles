fb5204c7cbb304860e2af41d68173a62535e6a62
DumbDane <lrpe@itu.dk>
2025-03-21 22:32:21 +0100

    Add blender

---
+++ nix/modules/blender.nix
@@ -0,0 +1,12 @@
+{ config, lib, pkgs, ... }:
+{
+    options = {
+        blender.enable = lib.mkEnableOption "Enables blender";
+    };
+
+    config = lib.mkIf config.blender.enable {
+        environment.systemPackages = with pkgs; [ blender ];
+        #programs.blender.enable = true;
+    };
+
+}
